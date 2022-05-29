a5b88efa44936d995c56fd7619ed787787331f9d
Prakhar Jalan <iit2020200@iiita.ac.in>
2022-05-29 14:46:36 +0530

    Completed useEffect Project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react'
-import UseEffect from './components/UseEffect'
-
+import Home from './components/Home'
+import './App.css'
 function App() {
   return (
-    <>
-      <h3>
-        Use states are used when while changing a state we need to change something outside the scope of the function ( SideEffects )
-      </h3>
-      <UseEffect/>
-
-    </>
+  <>
+    
+    <Home/>
+    {/* <Home/> */}
+  </>
   )
 }
 
